400ee6e08a04cbd6db19f169589384ada7ca32b4
Khang <khangdn95@gmail.com>
2024-04-15 02:21:20 +0000

    added exercises

---
+++ simple_shell_exercise/0-av.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print all args without argc
+ * @argc: void
+ * @argv: argument vector
+ * Return: all args
+ */
+int main(int __attribute__((unused)) argc, char **argv)
+{
+printf("%s\n", *argv);
+return (0);
+}
