8d878928eccb0ce3b9627e319bd8b67adf7505ea
NickelannG <8836@holbertonstudents.com>
2024-04-16 06:26:39 +0000

    super simple shell

---
+++ simple_shell_exercise/ss_shell.c
@@ -0,0 +1,73 @@
+#include "shell_exercise.h"
+
+/**
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+	char *buffer = malloc(1024);
+	size_t len = 1024;
+	char *token;
+	char *args[10];
+	int j;
+
+	if (buffer == NULL)
+	{
+		printf("Error: failed to allocate memory\n");
+		return (1);
+	}
+
+	while (1)
+	{
+		printf("$ ");
+		if (getline(&buffer, &len, stdin) == -1)
+		{
+			printf("Error: failed to read input\n");
+			free(buffer);
+			return (1);
+		}
+		token = strtok(buffer, " ");
+		j = 0;
+		
+		while (token)
+		{
+			args[j++] = token;
+			token = strtok(NULL, " ");
+		}
+		args[j] = NULL;
+		
+		pid_t pid;
+		pid = fork();
+		
+		if (pid == -1)
+		{
+			perror("Error: fork failed");
+			free(buffer);
+			return (1);
+		}
+		else if (pid == 0)
+		{
+			if (execve(args[0], args, NULL) == -1)
+			{
+				perror("Error: execute failed");
+				free(buffer);
+				return (1);
+			}
+		}
+		else
+		{
+			wait(NULL);
+		}
+
+		int i = 0;
+		while ( i < 10)
+		{
+			args[i++] = NULL;
+		}
+	}
+	free (buffer);
+	return (0);
+}
