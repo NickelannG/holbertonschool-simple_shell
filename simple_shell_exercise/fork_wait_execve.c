5740f76b36ef770e9d57f52a1e8061847c735234
Carole <c.zenruffinen@hotmail.com>
2024-04-16 03:45:44 +0000

    fork_wait_execve.c

---
+++ simple_shell_exercise/fork_wait_execve.c
@@ -0,0 +1,36 @@
+#include "ss_lib.h"
+
+/**
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+	pid_t pid;
+	int i = 0;
+
+	while (i < 5)
+	{
+		pid = fork();
+
+		if (pid == -1)
+		{
+			return (-1);
+		}
+		if (pid == 0)
+		{
+			char *array[] = {"/bin/ls", "-l", "/tmp", NULL};
+			execve(array[0], array, NULL);
+			exit (1);
+		 }
+		else
+		{
+			wait(NULL);
+		}
+		i++;
+
+	}
+	return (0);
+}
