6b5cebb298bd9d30d1638de3e75122d7c63e1c8d
NickelannG <8836@holbertonstudents.com>
2024-04-19 12:37:27 +0000

    updated setenv

--- simple_shell_exercise/5-setenv.c
+++ simple_shell_exercise/5-setenv.c
@@ -1,91 +1,144 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "shell_exercise.h"
 
-char **environ;
+extern char **environ;
 
-// Function to change the value of an existing environment variable
-void changeEnvVar(char *var, char *value) {
-    unsigned int i;
-    char *temp, *token, *final = "";
+/**
+ * changeEnvVar - a function that changes the value of an existing environment variable
+ *
+ * @var: a pointer to the variable name string that we want to change
+ * @value: a pointer to the value string of the environment variable name that we want to change to
+ * 
+ * Return: no return
+ */
 
-    i = 0;
-    while (environ[i] != NULL) {
-        temp = malloc(strlen(environ[i]) + 1);
-        strcpy(temp, environ[i]);
+void changeEnvVar(const char *name, const char *value) 
+{
+	unsigned int i;
 
-        token = strtok(temp, "=");
-        if (strcmp(token, var) == 0) {
-            final = malloc(strlen(token) + strlen("=") + strlen(value) + 1);
-            strcpy(final, token);
-            strcat(final, "=");
-            strcat(final, value);
-            environ[i] = malloc(strlen(final) + 1);
-            strcpy(environ[i], final);
-            free(temp);
-            free(final);
-            return; // Variable found and updated, no need to continue
+	if (name == NULL || value == NULL)
+        return;
+
+	i = 0;
+	while (environ[i] != NULL) 
+	{
+		temp = malloc(strlen(env[i]) + 1);
+		strcpy(temp,env[i]);
+
+		token = strtok(environ[i], "=");
+		if (strcmp(token, name) == 0) 
+		{
+			final = malloc(strlen(token) + strlen("=") +  strlen(myvalue) + 1);
+			strcpy(final, token);
+			strcat(final, "=");
+			strcat(final, myvalue);
+			env[i] = malloc(strlen(final) + 1);
+			env[i] =  final;
+			j = i;
+		}
+		i++;
+		free(temp);
+	}
+
+
+/**
+ * addEnVar - a function that adds a new environment variable
+ *
+ * @var: a pointer to the variable name string that we want to change
+ * @value: a pointer to the value string of the environment variable name that we want to change to
+ *
+ * Return: no return
+ */
+
+void addEnvVar(const char *name, const char *value) 
+{
+    if (name == NULL || value == NULL)
+        return;
+
+     int num_vars = 0;
+    while (environ[num_vars] != NULL)
+        num_vars++;
+
+    char **new_environ = malloc(sizeof(char *) * (num_vars + 2));
+    if (new_environ == NULL)
+        return;
+
+    int i = 0;
+    while (i < num_vars) {
+        new_environ[i] = strdup(environ[i]);
+        if (new_environ[i] == NULL) {
+            for (int j = 0; j < i; j++)
+                free(new_environ[j]);
+            free(new_environ);
+            return;
         }
         i++;
-        free(temp);
-    }
-}
-
-// Function to add a new environment variable
-void addEnvVar(char *var, char *value) {
-    unsigned int i;
-
-    i = 0;
-    while (environ[i] != NULL) {
-        i++;
     }
 
-    char **new_environ = malloc(sizeof(char *) * (i + 2)); // +2 for new variable and NULL terminator
-    for (int j = 0; j < i; j++) {
-        new_environ[j] = malloc(strlen(environ[j]) + 1);
-        strcpy(new_environ[j], environ[j]);
+    new_environ[num_vars] = malloc(strlen(name) + strlen(value) + 2);
+    if (new_environ[num_vars] == NULL) {
+        for (int j = 0; j < num_vars; j++)
+            free(new_environ[j]);
+        free(new_environ);
+        return;
     }
-    new_environ[i] = malloc(strlen(var) + strlen("=") + strlen(value) + 1);
-    sprintf(new_environ[i], "%s=%s", var, value);
-    new_environ[i + 1] = NULL; // NULL terminator
+    sprintf(new_environ[num_vars], "%s=%s", name, value);
+    new_environ[num_vars + 1] = NULL;
+
     environ = new_environ;
 }
 
-int main(int argc, char **argv, char **env) {
-    unsigned int i;
+/**
+ * _setenv - changes or adds an environment variable (without using setenv)
+ *
+ * @name: a pointer to a null terminated string containing the name of the
+ * environment variable to be set or updated
+ *
+ * @value: A pointer to a null-terminated string containing the new value 
+ * to be assigned to the environment variable
+ *
+ * @overwrite: an int flag to override the change
+ *
+ * Return: 0 on success, or -1 on failure
+ */
 
-    // Copying the environment variables
-    i = 0;
-    while (env[i] != NULL)
-        i++;
+int _setenv(const char *name, const char *value, int overwrite) 
+{
+	if (name == NULL || value == NULL)
+        	return -1; // Invalid input 
+	int i = 0;
+	while (environ[i] != NULL) 
+	{
+		char *var = strtok(environ[i], "=");
+		if (strcmp(var, name) == 0)
+		{
+			// Variable exists
+			if (overwrite)
+			changeEnvVar(name, value);
+			return 0;
+		}
+		i++;
+	}
+}
 
-    environ = malloc(sizeof(char *) * (i + 1)); // +1 for NULL terminator
-    for (int j = 0; j < i; j++) {
-        environ[j] = malloc(strlen(env[j]) + 1);
-        strcpy(environ[j], env[j]);
-    }
-    environ[i] = NULL; // NULL terminator
+/**
+ * Description: to test out the function
+ * main - entry point 
+ *
+ * Return: always 0
+ */
 
-    // Change the value of the environment variable MY_VAR or add it if it doesn't exist
-    changeEnvVar("LS_COLORS", "MYVALUE");
+int main() 
+{
+	// Test the _setenv function
+    _setenv("TEST_VAR", "initial_value", 0); // Add a new variable
 
-    // If MY_VAR doesn't exist, add it
-    if (environ[i - 1] != NULL && strncmp(environ[i - 1], "LS_COLORS=", strlen("LS_COLORS=")) != 0) {
-        addEnvVar("LS_COLORS", "MYVALUE");
-    }
-
-    // Printing the environment
-    i = 0;
+    // Print the environment to verify changes
+    int i = 0;
     while (environ[i] != NULL) {
-        printf("%s\n", environ[i]);
+        char *env_pair = environ[i];
+        printf("%s\n", env_pair);
         i++;
     }
-
-    // Freeing memory
-    for (int j = 0; environ[j] != NULL; j++)
-        free(environ[j]);
-    free(environ);
 
     return 0;
 }
-
