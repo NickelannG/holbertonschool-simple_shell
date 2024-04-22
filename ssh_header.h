502842454602f52eccfe06104694d92672caaef5
NickelannG <8836@holbertonstudents.com>
2024-04-22 23:43:14 +0000

    merged the other files from main

--- ssh_header.h
+++ ssh_header.h
@@ -7,10 +7,12 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
 void parse_command(char *command, char *args[]);
 void execute_command(char *command, char *args[]);
 void non_interactive_mode();
 void interactive_mode(void);
+void find_path(char *final);
 
 #endif
