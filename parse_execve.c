b9949a1a4583344fed7d6fd4f2a8b479d9590e51
NickelannG <8836@holbertonstudents.com>
2024-04-22 23:42:16 +0000

    merged parse_execve file

--- parse_execve.c
+++ parse_execve.c
@@ -21,6 +21,44 @@ token = strtok(NULL, delimeter);
 args[argc] = NULL;
 }
 
+void find_path(char *final)
+{
+char *pathvar = getenv("PATH");
+ char *token, *cmd = "/ls";
+    struct stat st;
+
+    if (pathvar == NULL) 
+    {
+        fprintf(stderr, "Error: PATH environment variable not found\n");
+        return;
+    }
+
+    token = strtok(pathvar, ":");
+    while (token != NULL) 
+    {
+        final = malloc(strlen(token) + strlen(cmd) + 1);
+        if (final == NULL) 
+	{
+            fprintf(stderr, "Error: Memory allocation failed\n");
+            return;
+        }
+        strcpy(final, token);
+        strcat(final, cmd);
+
+        printf("Status of %s: ", final);
+        if (stat(final, &st) == -1) 
+	{
+	  fprintf(stderr, "Error: PATH environment variable not found\n");
+	  free(final);
+        }
+	else 
+	{
+	  return;
+        }
+        token = strtok(NULL, ":");
+    }
+}
+
 /**
  * execute_command - to execute new process
  * by replacing the previous
@@ -29,8 +67,14 @@ args[argc] = NULL;
  */
 void execute_command(char *command, char *args[])
 {
+extern char **environ;
 pid_t pid;
 int status;
+if (command == NULL)
+{
+return;
+}
+find_path(command);
 pid = fork();
 if (pid == -1)
 {
@@ -39,9 +83,9 @@ exit(EXIT_FAILURE);
 }
 else if (pid == 0)
 {
-if (execvp(command, args) == -1)
+if (execve(command, args, environ) == -1)
 {
-perror("execvp");
+perror("execve");
 exit(EXIT_FAILURE);
 }
 }
