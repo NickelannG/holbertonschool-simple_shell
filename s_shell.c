260457c63299765d42f6f28ca32cf116db89087f
Khang <khangdn95@gmail.com>
2024-04-22 01:04:57 +0000

    updated s_shell.c to betty format with 3 functions

--- s_shell.c
+++ s_shell.c
@@ -1,29 +1,18 @@
 #include "ssh_header.h"
 #define MAX_COMMAND_LENGTH 100
 /**
- * main - simple shell interative entry point
- * Return: Always 0 (Success)
+ * parse_command - to tokenise the user's input
+ * @command: the string by the user's input
+ * @args: the array of strings representing the parsed
+ * arguments
  */
-int main(void)
+void parse_command(char *command, char *args[])
 {
-char *command = NULL;
 size_t len = 0;
-char *args[] = {NULL};
 char *delimeter = " \n";
 char *token;
-pid_t pid;
-int status;
-while (1)
-{
-printf("$ ");
-fflush(stdout);
-if (getline(&command, &len, stdin) == -1)
-{
-printf("\n");
-free(command);
-break;
-}
 int argc = 0;
+
 token = strtok(command, delimeter);
 while (token != NULL && argc < MAX_COMMAND_LENGTH)
 {
@@ -31,18 +20,29 @@ args[argc++] = token;
 token = strtok(NULL, delimeter);
 }
 args[argc] = NULL;
+}
+
+/**
+ * execute_command - to execute new process
+ * by replacing the previous
+ * @command: the path of the command
+ * @args: command arguments or flags
+ */
+void execute_command(char *command, char *args[])
+{
+pid_t pid;
+int status;
 pid = fork();
 if (pid == -1)
 {
 perror("fork");
-free(command);
 exit(EXIT_FAILURE);
 }
 else if (pid == 0)
 {
-if (execv(command, args) == -1)
+if (execvp(command, args) == -1)
 {
-perror("execv");
+perror("execvp");
 exit(EXIT_FAILURE);
 }
 }
@@ -51,10 +51,32 @@ else
 if (wait(&status) == -1)
 {
 perror("wait");
-free(command);
 exit(EXIT_FAILURE);
 }
 }
+}
+
+/**
+ * main - simple shell interative entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char *command = NULL;
+char *args[MAX_COMMAND_LENGTH + 1] = {NULL};
+while (1)
+{
+printf("$ ");
+fflush(stdout);
+size_t len = 0;
+if (getline(&command, &len, stdin) == -1)
+{
+printf("\n");
+free(command);
+break;
+}
+parse_command(command, args);
+execute_command(args[0], args);
 }
 free(command);
 return (0);
