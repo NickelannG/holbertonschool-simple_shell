d4ee322c1c55f5685b1a7b0ccc6ee806da23ca17
NickelannG <8836@holbertonstudents.com>
2024-04-23 00:11:42 +0000

    merged parse_execve and header files

--- parse_execve.c
+++ parse_execve.c
@@ -21,43 +21,36 @@ token = strtok(NULL, delimeter);
 args[argc] = NULL;
 }
 
-void find_path(char *final)
-{
-char *pathvar = getenv("PATH");
- char *token, *cmd = "/ls";
+char *find_path(char *command) {
+    char *pathvar = getenv("PATH");
+    char *token, *path_with_cmd;
     struct stat st;
 
-    if (pathvar == NULL) 
-    {
+    if (pathvar == NULL) {
         fprintf(stderr, "Error: PATH environment variable not found\n");
-        return;
+        return NULL;
     }
 
     token = strtok(pathvar, ":");
-    while (token != NULL) 
-    {
-        final = malloc(strlen(token) + strlen(cmd) + 1);
-        if (final == NULL) 
-	{
+    while (token != NULL) {
+        path_with_cmd = malloc(strlen(token) + strlen(command) + 2); /* +2 for '/' and '\0' */
+        if (path_with_cmd == NULL) {
             fprintf(stderr, "Error: Memory allocation failed\n");
-            return;
+            return NULL;
         }
-        strcpy(final, token);
-        strcat(final, cmd);
+        sprintf(path_with_cmd, "%s/%s", token, command);
 
-        printf("Status of %s: ", final);
-        if (stat(final, &st) == -1) 
-	{
-	  fprintf(stderr, "Error: PATH environment variable not found\n");
-	  free(final);
+        if (stat(path_with_cmd, &st) == 0 && S_ISREG(st.st_mode) && (st.st_mode & S_IXUSR)) {
+            return path_with_cmd;
         }
-	else 
-	{
-	  return;
-        }
+
+        free(path_with_cmd);
         token = strtok(NULL, ":");
     }
+
+    return NULL; /* Command not found in any directory in PATH */
 }
+
 
 /**
  * execute_command - to execute new process
@@ -65,36 +58,38 @@ char *pathvar = getenv("PATH");
  * @command: the path of the command
  * @args: command arguments or flags
  */
-void execute_command(char *command, char *args[])
+void execute_command(char *command, char *args[]) 
 {
-extern char **environ;
-pid_t pid;
-int status;
-if (command == NULL)
-{
-return;
-}
-find_path(command);
-pid = fork();
-if (pid == -1)
-{
-perror("fork");
-exit(EXIT_FAILURE);
-}
-else if (pid == 0)
-{
-if (execve(command, args, environ) == -1)
-{
-perror("execve");
-exit(EXIT_FAILURE);
-}
-}
-else
-{
-if (wait(&status) == -1)
-{
-perror("wait");
-exit(EXIT_FAILURE);
-}
-}
+	char *full_path;
+	extern char **environ;
+	pid_t pid;
+	int status;
+
+	if (command == NULL) {
+        return;
+    }
+
+    full_path = find_path(command);
+    if (full_path == NULL) {
+        fprintf(stderr, "Error: Command '%s' not found\n", command);
+        return;
+    }
+    
+    pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        if (execve(full_path, args, environ) == -1) {
+            perror("execve");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        if (wait(&status) == -1) {
+            perror("wait");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    free(full_path);
 }
