8e1aa6be40d4415d29f92d0c85f2a1203d99880d
Carole <c.zenruffinen@hotmail.com>
2024-04-26 21:42:07 +0000

    updated find_path

--- find_path.c
+++ find_path.c
@@ -6,13 +6,16 @@
  * Return: cmd path
 */
 
+/**
 char *find_path(char *command)
 {
-	char *path = _getenv("PATH"), *path_cpy;
+	char *path, *path_cpy;
 	char **path_split;
 	char *path_concat = NULL;
 	int i = 0, path_len = 0;
 	struct stat info;
+
+	path = _getenv("PATH");
 
 	if (stat(command, &info) == 0)
 		return (command);
@@ -47,3 +50,55 @@ char *find_path(char *command)
 	free(path_split);
 	return (path_concat);
 }
+
+*/
+
+char *find_path(char *command)
+{
+    char *path, *path_cpy, **path_split, *path_concat = NULL;
+    int i = 0, path_len = 0;
+    struct stat info;
+
+    path = _getenv("PATH");
+    if (stat(command, &info) == 0)
+        return command;
+
+    path_cpy = malloc(strlen(path) + 1);
+    strcpy(path_cpy, path);
+
+    path_split = parse(path_cpy, ":");
+    if (!path_split)
+    {
+        free(path_cpy);
+        return NULL;
+    }
+
+    path_concat = malloc(path_len + 1);
+    if (!path_concat)
+    {
+        free(path_cpy);
+        free(path_split);
+        return NULL;
+    }
+
+    for (i = 0; path_split[i] != NULL; i++)
+    {
+        strcpy(path_concat, path_split[i]);
+        if (path_concat[strlen(path_concat) - 1] != '/')
+            strcat(path_concat, "/");
+        strcat(path_concat, command);
+
+        if (stat(path_concat, &info) == 0)
+        {
+            free(path_cpy);
+            free(path_split);
+            return path_concat;
+        }
+    }
+
+    free(path_cpy);
+    free(path_split);
+    free(path_concat);
+    return NULL;
+}
+
