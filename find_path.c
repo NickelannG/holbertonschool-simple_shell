9256821d172daac4e1d8465530d0437fbecbb402
Carole <c.zenruffinen@hotmail.com>
2024-04-26 21:55:13 +0000

    added frees to avoid memory leaks

--- find_path.c
+++ find_path.c
@@ -55,50 +55,52 @@ char *find_path(char *command)
 
 char *find_path(char *command)
 {
-    char *path, *path_cpy, **path_split, *path_concat = NULL;
-    int i = 0, path_len = 0;
+    char *path, *path_cpy, **path_split, *path_concat;
+    int i;
+    size_t path_len, command_len;
     struct stat info;
 
     path = _getenv("PATH");
+    if (!path)
+        return NULL;
+
     if (stat(command, &info) == 0)
         return command;
 
     path_cpy = malloc(strlen(path) + 1);
+    if (!path_cpy)
+        return NULL;
     strcpy(path_cpy, path);
 
     path_split = parse(path_cpy, ":");
+    free(path_cpy);
     if (!path_split)
-    {
-        free(path_cpy);
         return NULL;
-    }
-
-    path_concat = malloc(path_len + 1);
-    if (!path_concat)
-    {
-        free(path_cpy);
-        free(path_split);
-        return NULL;
-    }
 
     for (i = 0; path_split[i] != NULL; i++)
     {
+        path_len = strlen(path_split[i]);
+        command_len = strlen(command);
+        path_concat = malloc(path_len + command_len + 2);
+        if (!path_concat)
+        {
+            free(path_split);
+            return NULL;
+        }
         strcpy(path_concat, path_split[i]);
-        if (path_concat[strlen(path_concat) - 1] != '/')
+        if (path_concat[path_len - 1] != '/')
             strcat(path_concat, "/");
         strcat(path_concat, command);
 
         if (stat(path_concat, &info) == 0)
         {
-            free(path_cpy);
             free(path_split);
             return path_concat;
         }
+        free(path_concat);
     }
 
-    free(path_cpy);
     free(path_split);
-    free(path_concat);
     return NULL;
 }
 
